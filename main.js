590007ec4d6a030d4895097eb864a3bf09470dbf
Mas Adi <githubmasadi@gmail.com>
2025-03-07 16:22:12 +0700

    main.js

--- main.js
+++ main.js
@@ -3,31 +3,30 @@ const api = "https://api.genderize.io";
 
 function showResult(name, gender, probability) {
     const predic = document.getElementById('predic');
-    const procen = probability * 100 // Format probabilitas menjadi 2 angka desimal
-    let genderDec = ""; // Variabel untuk menyimpan hasil gender yang lebih ramah pengguna
-
-    // Mengganti istilah gender dari API dengan istilah yang lebih sesuai
+    const procen = probability * 100
+    let genderDec = "";
+    
     if (gender === "male") {
         genderDec = "cowok";
     } else if (gender === "female") {
         genderDec = "cewek";
     } else {
-        genderDec = "tidak terdefinisi"; // Tangani kasus di mana gender tidak terdefinisi
+        genderDec = "tidak terdefinisi"; 
     }
 
     const preText = `Halo ${name}, jenis kelamin kamu kemungkinan adalah ${genderDec} sebesar ${procen}%`;
-    predic.textContent = preText; // Tampilkan hasil
+    predic.textContent = preText;
 }
 
 async function predict(event) { 
     if (event.key === "Enter") {
-        const user = event.target.value.trim(); // Hapus spasi dari input
-        if (user) { // Cek jika input pengguna tidak kosong
+        const user = event.target.value.trim(); 
+        if (user) {
             const QueryUrl = `${api}/?name=${user}&country_id=ID`;
 
             try {
                 const res = await fetch(QueryUrl);
-                if (!res.ok) { // Cek jika respons OK
+                if (!res.ok) { 
                     throw new Error('Respons jaringan tidak baik');
                 }
                 const result = await res.json();
